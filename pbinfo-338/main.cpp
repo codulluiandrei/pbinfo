425be9d4af82660442057ae404abbb066d6a1614
Andrew <frintu.andrei07@gmail.com>
2022-11-01 21:26:49 +0200

    actualizare problema

--- pbinfo-338/main.cpp
+++ pbinfo-338/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n; cin >> n;
-    long long S = 0;
-    for (int i = 1; i <= n; i++) {
-    	long long putere = 1;
-        for (int i2 = 1; i2 <= i; i2++)
-            putere = 1LL * putere * i;
-        S = S + putere;
-    }
-    cout << "Rezultatul este " << S;
+    int n;
+    cin >> n;
+    long long int S = 0, P;
+    for (int i = 1; i <= n; ++i) {
+        P = 1;
+        for (int j = 1; j <= i; ++j)
+            P = P * i;
+        S = S + P;
+    } cout << "Rezultatul este " << S;
     return 0;
 }
