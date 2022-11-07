67f624c4a60796ab54c044cb472c656dabf71e5f
Andrew <frintu.andrei07@gmail.com>
2022-11-07 22:33:00 +0200

    actualizare rezolvare

--- pbinfo-10/main.cpp
+++ pbinfo-10/main.cpp
@@ -1,15 +1,11 @@
-#include <bits/stdc++>
+#include <iostream>
 using namespace std;
-
-ifstream in("suma-cifrelor.in");
-ofstream out("suma-cifrelor.out");
-
 int main() {
-	int a, suma = 0;
-	in >> a;
-	while (a > 0) {
-		suma = suma + a % 10;
-		a = a / 10;
-	} out << suma;
-	return 0;
+	int n, S = 0;
+    cin >> n;
+    while (n > 0) {
+    	S = S + n % 10;
+        n = n / 10;
+    } cout << S;
+    return 0;
 }
