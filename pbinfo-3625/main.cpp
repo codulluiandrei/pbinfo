e9380fe3fe2c54f6491e2668135e05769c35b555
Andrew <frintu.andrei07@gmail.com>
2022-11-07 22:30:54 +0200

    actualizare rezolvare

--- pbinfo-3625/main.cpp
+++ pbinfo-3625/main.cpp
@@ -1,43 +1,23 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n, a = 0, b = 0, c = 0, d = 0, e = 1, f = 0; cin >> n;
-    int temp = n, temp2 = n, nrcif = 0, cifn[10001];
-    int cif1 = temp % 10;
-    while (temp != 0) {
-        nrcif++;
-        temp = temp / 10;
-    }
-    while (temp2 >= 10) {
-        temp2 = temp2 / 10;
-    } 
-    for (int i = 0; i < nrcif; i++) {
-        cifn[i] = n % 10;
+    int cif, n, a = 0, b = 0, c = 0, d = 0, e = 1, f;
+    cin >> n;
+    f = n % 10;
+    while (n > 0) {
+        cif = n % 10;
         n = n / 10;
-    }
-    for (int i = 0; i < nrcif; i++) {
-        a = cifn[i] * cifn[i] + a;
-    } cout << a << " ";
-    for (int i = 0; i < nrcif; i++) {
-        if (cifn[i] % 2 != 0) {
-            b = cifn[i] + b;
-        }
-    } cout << b << " ";
-    for (int i = 0; i < nrcif; i++) {
-        if (cifn[i] % 3 == 0) {
-            c = cifn[i] + c;
-        }
-    } cout << c << " ";
-    for (int i = 0; i < nrcif; i++) {
-        if (cifn[i] > 5) {
+        a = a + cif * cif;
+        if (cif % 2 == 1)
+            b = b + cif;
+        if (cif % 3 == 0)
+            c = c + cif;
+        if (cif > 5)
             d++;
-        }
-    } cout << d << " ";
-    for (int i = 0; i < nrcif; i++) {
-        if (cifn[i] > 0) {
-            e = cifn[i] * e;
-        }
-    } cout << e << " ";
-    f = cif1 + temp2; cout << f;
+        if (cif != 0)
+            e = e * cif;
+        if (n < 10) 
+            f = f + n;
+    } cout << a << " " << b << " " << c << " " << d << " " << e << " " << f;
     return 0;
 }
