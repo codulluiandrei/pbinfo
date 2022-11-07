1fc36062089a9d41cc8748eefe959db7666beac8
Andrew <frintu.andrei07@gmail.com>
2022-11-07 21:20:40 +0200

    actualizare rezolvare

--- pbinfo-347/main.cpp
+++ pbinfo-347/main.cpp
@@ -1,14 +1,14 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main() {
-	int n, temp;
- 	cin >> n;
- 	int* v = new int[n];
- 	for (int i = 0; i < n; i++) {
-  		cin >> temp;
-  		v[i] = temp;
- 	}
-	sort(v, v + n);
-	cout << v[0] + v[n-1];
+    int n, temp, maxim, minim;
+    cin >> n;
+    cin >> temp;
+    minim = maxim = temp;
+    for (int i = 2; i <= n; ++i) {
+        cin >> temp;
+        if (temp > maxim) maxim = temp;
+        if (temp < minim) minim = temp;
+    } cout << maxim + minim;
     return 0;
 }
