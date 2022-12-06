5184c720d688c600bed67b68a0b43929f3fbc657
Andrew <frintu.andrei07@gmail.com>
2022-12-06 07:57:55 +0200

    actualizare rezolvarea

--- pbinfo-375/main.cpp
+++ pbinfo-375/main.cpp
@@ -1,21 +1,17 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main() {
-	int n; cin>>n;
-    if (n == 1) {
-        cout << 2;
-        return 0;
-    }
-    if (n % 2 == 0) n++;
-    else n += 2;
-    while(true) {
-            if (n > 1 && (n % 2 != 0 || n == 2)) {
-                    int radical = sqrt(n), temp;
-                    for(temp = 3; temp <= radical; temp += 2)
-                        if(n % temp == 0)
-                            break;
-                    if(temp > radical)
-                        break;
-                } n += 2;
-        } cout << n;
+    int n;
+    bool prim = false;
+    cin >> n;
+    while (!prim) {
+        n++;
+        prim = true;
+        if (n < 2) prim = false;
+        if (n % 2 == 0 && n > 2) prim = false;
+        for (int d = 3; d * d <= n; d = d + 2)
+            if (n % d == 0) prim = false;
+            
+    } cout << n;
+    return 0;
 }
