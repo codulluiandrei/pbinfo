078f5260f2b39658ee0a7829a0a3cec557862bc4
Andrew <frintu.andrei07@gmail.com>
2023-01-08 18:25:09 +0200

    actualizare rezolvare

--- pbinfo-79/main.cpp
+++ pbinfo-79/main.cpp
@@ -2,6 +2,6 @@ void afismat(int a[][100], int n, int m) {
     for (int i = 0; i < n; i++) {
     	for (int j = 0; j < m; j++)
     		cout << a[i][j] << " ";
-    	cout << endl;
+    	cout << "\n";
     }
 }
