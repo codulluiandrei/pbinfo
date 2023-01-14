f784642dd2ea317dd2d4d576e8a3487753e4b953
Andrew <frintu.andrei07@gmail.com>
2023-01-14 11:14:35 +0200

    modificare rezolvare

--- pbinfo-777/main.cpp
+++ pbinfo-777/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int n, m, mat[101][101], rez = 0;
+int n, m, mat[101][101], rezultat = 0;
 int main() {
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
@@ -12,7 +12,7 @@ int main() {
             for (int l = i + 1; l <= n; l++)
                 if (mat[i][j] == mat[l][j])
                     adv = false;
-        if (adv == true) rez++;
-    } cout << rez;
+        if (adv == true) rezultat++;
+    } cout << rezultat;
     return 0;
 }
