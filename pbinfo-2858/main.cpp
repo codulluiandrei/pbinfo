36a45ac4fdc4a5a4f0f4fbd1c390237c9073ec8b
Andrew <frintu.andrei07@gmail.com>
2023-01-30 10:18:20 +0200

    modificare rezolvare

--- pbinfo-2858/main.cpp
+++ pbinfo-2858/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int v[101], n, copie[101], Spar = 0, Sppar = 0, nrdiv = 0, Simp = 0;
-	cin >> n;
-	for (int i = 1; i <= n; ++i)
-		cin >> v[i];
-	int temp = n;
-	for (int i = 1; i <= n; ++i) {
-		copie[i] = v[temp];
-		temp--;
-	} for (int i = 1; i <= n; ++i) {
-		cout << copie[i] << " ";
-	} cout << "\n";
-	for (int i = 1; i <= n; ++i) {
-		if (v[i] % 2 == 0) Spar = Spar + v[i];
-		if (i % 2 == 0) Sppar = Sppar + v[i];
-		if (v[i] % 10 == 0) nrdiv++;
-		if (i % 2 != 0 && v[i] % 3 == 0) Simp = Simp + v[i];
-	} cout << Spar << "\n";
-	cout << Sppar << "\n";
-	cout << nrdiv << "\n";
-	cout << Simp << "\n";
-	return 0;
+    int v[101], n, div2 = 0, poz2 = 0, div10 = 0, div3poz1 = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> v[i];
+    for(int i = n; i > 0; i--)
+        cout << v[i] << " ";
+    cout << "\n";
+    for (int i = 1; i <= n; i++) {
+        if (v[i] % 2 == 0)
+            div2 = div2 + v[i];
+        if (i % 2 == 0)
+            poz2 = poz2 + v[i];
+        if (v[i] % 10 == 0)
+            div10++;
+        if (v[i] % 3 == 0 && i % 2 == 1)
+            div3poz1 = div3poz1 + v[i];
+    }
+    cout << div2 << "\n";
+    cout << poz2 << "\n";
+    cout << div10 << "\n";
+    cout << div3poz1;
+    return 0;
 }
