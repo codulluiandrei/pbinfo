9438e495faaf849927f177e0479c484a1e6848a9
Andrew <frintu.andrei07@gmail.com>
2025-01-29 15:23:08 +0200

    rezolvare modificata

--- pbinfo-4037/main.cpp
+++ pbinfo-4037/main.cpp
@@ -1,40 +1,42 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int a[1002], n, x, k, i, p;
-    cin >> n;
-    for (i = 1; i <= n; i++)
-        cin >> a[i];
-    cin >> x >> k;
-
-    /// cea mai din stanga pozitie p a lui x
-    for (p = 1; p <= n && a[p] != x; p++)
-        ;
-    if (p > n)
-    {
-        cout << "0\n0\n0\n";
-        return 0;
-    }
-    cout << p << "\n";
-
-    /// cea mai din dreapta pozitie p a lui x
-    for (p = n; p >= 1 && a[p] != x; p--)
-        ;
-    cout << p << "\n";
-
-    /// a k-a aparitie a lui x in a:
-    for (i = 1; i <= n; i++)
-        if (a[i] == x)
-        {
-            k--;
-            if (k == 0)
-            {
-                cout << i << "\n";
-                return 0;
-            }
-        }
-    cout << "0\n";
-    return 0;
-}
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long int n,v[2001], x , k , b , a , nr = 0 , c , cnt = 0;
+
+int main()
+{
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>v[i];
+    }
+    cin>>x>>k;
+    for(int i=1;i<=n;i++)
+    {
+        if(v[i]==x && nr==0)
+        {
+            a=i;
+            nr++;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(v[i]==x)
+        {
+            b=i;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(v[i]==x && cnt<k)
+        {
+            c=i;
+            cnt++;
+        }
+    }
+    if(nr==0) cout<<‘0’<<endl<<‘0’<<endl;
+    else cout<<a<<endl<<b<<endl;
+    if(cnt<k) cout<<‘0’;
+    else cout<<c;
+}
